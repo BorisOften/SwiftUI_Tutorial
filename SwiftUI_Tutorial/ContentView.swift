abe3609e68a1e31efe209eee82a65a9f324cbbf5
Boris <banofon@gmail.com>
2023-04-16 08:48:51 -0400

    Initial commit

---
+++ SwiftUI_Tutorial/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUI_Tutorial
+//
+//  Created by Boris Ofon on 4/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("This is a test")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI_Tutorial
+//
+//  Created by Boris Ofon on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
